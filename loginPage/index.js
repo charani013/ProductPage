1cff30981be2fbba2ce8dfc7eaf4f2e39c581e65
charani013 <90957976+charani013@users.noreply.github.com>
2021-10-11 00:22:32 -0700

    Update index.js

--- loginPage/index.js
+++ loginPage/index.js
@@ -5,7 +5,7 @@ function validated() {
     let username = document.forms['form']['username'].value;
     let password = document.forms['form']['password'].value;
     if (username.length >=4 && password.length >=4) {
-        window.location.href = "https://charani013.github.io/homepage/product/products.html"
+        window.location.href = "https://charani013.github.io/ProductPage/homePage/product.html"
     } else {
         alert("Enter username and password maximum 4 characters")
     }
